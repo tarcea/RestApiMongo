d27744ab20200fe748d15f7efc056859f98d5a6f
tarcea <tarcea@gmail.com>
2021-05-03 14:18:56 +0200

    added add form

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 // All authors
 router.get('/', (req, res) => {
@@ -8,11 +9,11 @@ router.get('/', (req, res) => {
 
 // New author
 router.get('/new', (req, res) => {
-  res.render('authors/new')
+  res.render('authors/new', { author: new Author() })
 })
 
 // Create an author
-router.post('/', (res, req) => {
-  res.setEncoding("create")
+router.post('/', (req, res) => {
+  res.send("create")
 })
 module.exports = router
